2504e53bbbcc9912bc946c4b8f31ab9e4d6240c5
txwei3 <twei3@andrew.cmu.edu>
2022-09-21 23:47:58 -0400

    Hw3 updated with objects

--- solution-hw3/js/detail-js.js
+++ solution-hw3/js/detail-js.js
@@ -1,46 +1,70 @@
-// geting text from a dropdown menu
-// https://stackoverflow.com/questions/5913/getting-the-text-from-a-drop-down-box
+//creating objects
+const glaze = {
+    option: ["Keep Original", "Sugar Milk", "Vanilla Milk", "Double Chocolate"],
+    adaption: [0, 0, 0.5, 1.5],
+}
 
-function getPackSize() {
-    let selectPackSize = document.getElementById("pack-size");
-    let selectSize = selectPackSize.options[selectPackSize.selectedIndex].text;
-    let sizeAdaption = 1;
-    if (selectSize == 1) {
-        sizeAdaption = 1;
-    }
-
-    else if (selectSize == 3) {
-        sizeAdaption = 3;
-    }
-
-    else if (selectSize == 6) {
-        sizeAdaption = 5;
-    }
-
-    else if (selectSize == 12) {
-        sizeAdaption = 10;
-    }
-
-    //console.log("this is sizeAdaption", sizeAdaption);
-    return sizeAdaption;
+const size = {
+    option: ["1", "3", "6", "12"],
+    adaption: [1, 3, 5, 10],
 }
 
 
+
+//setting fields using created objects
+//https://stackoverflow.com/questions/8674618/adding-options-to-select-with-javascript
+let selectGlaze = document.getElementById("glaze-option");
+let selectSize = document.getElementById("pack-size");
+
+for (i = 0; i < glaze.option.length; i++) {
+    let elem = document.createElement('option');
+    elem.value = glaze.option[i];
+    elem.innerHTML = glaze.option[i];
+    selectGlaze.appendChild(elem);
+}
+
+
+for (i = 0; i < size.option.length; i++) { 
+    let elem = document.createElement('option');
+    elem.value = size.option[i];;
+    elem.innerHTML = size.option[i];;
+    selectSize.appendChild(elem);
+}
+
+
+//getting text from the fields and matching them to adaptions
+//https://stackoverflow.com/questions/5913/getting-the-text-from-a-drop-down-box
 function glazePriceAdjustment() {
     let selectGlazing = document.getElementById("glaze-option");
     let glazingText = selectGlazing.options[selectGlazing.selectedIndex].text;
+    // console.log(glazingText);
     let glazePriceAdaption = 0
-    if (glazingText == "Vanilla Milk") {
-        glazePriceAdaption = 0.5;
-    }
-
-    else if (glazingText == "Double Chocolate") {
-        glazePriceAdaption = 1.5;
+    for (i = 0; i < glaze.option.length; i++) {
+        // console.log("hello")
+        if (glaze.option[i].toLocaleLowerCase() === glazingText.toLocaleLowerCase()) {
+            glazePriceAdaption = glaze.adaption[i];
+        }
     }
     //console.log("this is packAdaption", glazePriceAdaption)
     return glazePriceAdaption;
 }
 
+
+function getPackSize() {
+    let selectPackSize = document.getElementById("pack-size");
+    let selectSize = selectPackSize.options[selectPackSize.selectedIndex].text;
+    let sizePriceAdaption = 1;
+    for (i = 0; i < size.option.length; i++) {
+        if (size.option[i] === selectSize) {
+            sizePriceAdaption = size.adaption[i];
+        }
+    }
+
+    //console.log("this is sizeAdaption", sizeAdaption);
+    return sizePriceAdaption;
+}
+
+//final calculation
 function finalPrice() {
     let basePrice = 2.49;
     let glazeAdjustment = glazePriceAdjustment();
@@ -50,6 +74,3 @@ function finalPrice() {
     //console.log(typeof finalCost, finalCost)
     document.getElementById("total-cost").textContent = "$" + finalCost;    
 }
-
-
-
