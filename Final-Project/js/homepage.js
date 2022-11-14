21342b5b3d99f9ad1f066ad86569bc0f8536d80c
txwei3 <twei3@andrew.cmu.edu>
2022-11-14 11:08:51 -0500

    continue working on nav bar

--- Final-Project/js/homepage.js
+++ Final-Project/js/homepage.js
@@ -1,14 +1,33 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+
+let isNavOpen = false;
+
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("header").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("header").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("header").style.marginLeft = "0";
+}
+
+
+function navState() {
+  if (isNavOpen === false) {
+    openNav();
+    console.log("opening Nav");
+    isNavOpen = true;
   }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("header").style.marginLeft = "0";
+  else {
+    closeNav();
+    console.log("closing Nav");
+    isNavOpen = false
   }
+  return isNavOpen
+}
+
 
