c05a72cfc4e35410c2e0d097ffa7fdcb82364c9c
txwei3 <twei3@andrew.cmu.edu>
2022-11-14 23:38:53 -0500

    start dropdown for driver

--- Final-Project/js/homepage.js
+++ Final-Project/js/homepage.js
@@ -1,17 +1,16 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-
 let isNavOpen = false;
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  // document.getElementById("main").style.margin = "auto";
   document.getElementById("header").style.marginLeft = "250px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  // document.getElementById("main").style.marginLeft = "0";
   document.getElementById("header").style.marginLeft = "0";
 }
 
@@ -19,15 +18,17 @@ function closeNav() {
 function navState() {
   if (isNavOpen === false) {
     openNav();
-    console.log("opening Nav");
+    // console.log("opening Nav");
     isNavOpen = true;
   }
   else {
     closeNav();
-    console.log("closing Nav");
+    // console.log("closing Nav");
     isNavOpen = false
   }
   return isNavOpen
 }
 
+
+// ----------------------------
 
