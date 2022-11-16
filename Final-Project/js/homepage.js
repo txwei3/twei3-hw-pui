fbd8daddcc6de8be8a8924756fde7f4eac4ac8f1
txwei3 <twei3@andrew.cmu.edu>
2022-11-15 22:32:34 -0500

    template is working

--- Final-Project/js/homepage.js
+++ Final-Project/js/homepage.js
@@ -32,3 +32,66 @@ function navState() {
 
 // ----------------------------
 
+
+class Driver {
+  constructor(driverName, driverWins, driverPoints, driverPic, driverBio, interestingContent) {
+    this.name = driverName;
+    this.wins = driverWins;
+    this.points = driverPoints;
+    this.picture = driverPic; 
+    this.bio = driverBio;
+    this.content = interestingContent;
+
+    this.element = null;
+  }
+}
+
+let driverList = [];
+
+function addDriver(driverName, driverWins, driverPoints, driverPic, driverBio, interestingContent) {
+  const driver = new Driver(driverName, driverWins, driverPoints, driverPic, driverBio, interestingContent);
+  driverList.push(driver);
+  return driver;
+}
+
+function createElement(driver) {
+  let template = document.querySelector("#dropdown-template");
+  const clone = template.content.cloneNode(true);
+
+  driver.element = clone.querySelector(".dropdown");
+
+  const driverElementList = document.querySelector("#driver-list");
+  driverElementList.prepend(driver.element);
+
+  updateElement(driver);
+}
+
+function updateElement(driver) {
+  const name = driver.element.querySelector("#button-item-name");
+  const wins = driver.element.querySelector("#button-item-wins");
+  const points = driver.element.querySelector("#button-item-points");
+  const picture = driver.element.querySelector("#bio-img");
+  const bio = driver.element.querySelector("#bio-text");
+  const content = driver.element.querySelector("#interesting-content");
+
+  name.innerText = driver.name;
+  wins.innerText = driver.wins;
+  points.innerText = driver.points;
+  picture.src = driver.picture;
+  bio.innerText = driver.bio;
+  content.innerText = driver.content;
+}
+
+const mVer = addDriver("M. Verstappen", 14, 429, "./HW1-assets/driverPictures/m_Verstappen.jpg", "hello", "beunos dias")
+const cPer = addDriver("C. Perez", 14, 429, "./HW1-assets/driverPictures/m_Verstappen.jpg", "hello", "beunos dias")
+const cLec = addDriver("C. LeClerc", 14, 429, "./HW1-assets/driverPictures/m_Verstappen.jpg", "hello", "beunos dias")
+
+
+
+for (let i = 0; i < driverList.length; i++) {
+  console.log(driverList[i]);
+  createElement(driverList[i]);
+}
+
+
+
