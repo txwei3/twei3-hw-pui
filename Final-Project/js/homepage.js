d9ae36cc5289316dd8415020d954260a8356bcfa
txwei3 <twei3@andrew.cmu.edu>
2022-11-16 19:38:48 -0500

    continue working on api call
    api call works, template works, but content isn't display until functions are run in console

--- Final-Project/js/homepage.js
+++ Final-Project/js/homepage.js
@@ -1,37 +1,3 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-let isNavOpen = false;
-
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  // document.getElementById("main").style.margin = "auto";
-  document.getElementById("header").style.marginLeft = "250px";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  // document.getElementById("main").style.marginLeft = "0";
-  document.getElementById("header").style.marginLeft = "0";
-}
-
-
-function navState() {
-  if (isNavOpen === false) {
-    openNav();
-    // console.log("opening Nav");
-    isNavOpen = true;
-  }
-  else {
-    closeNav();
-    // console.log("closing Nav");
-    isNavOpen = false
-  }
-  return isNavOpen
-}
-
-
-// ----------------------------
-
 //---creating the dropdowns for the drivers---
 
 var settings = {
@@ -41,17 +7,16 @@ var settings = {
 };
 
 var positionList; // for the driver standings
-var infoList; // for the driver information
-
+var totalNumDrivers = 0; // total number of drivers
 
 $.ajax(settings).done(function (response) {
   //console.log(response);
-  //console.log(typeof(response))
-  let temp = response.MRData.StandingsTable.StandingsLists[0]
-  positionList = temp.DriverStandings
-  //console.log(typeof(positionList), positionList[0])
+  let temp = response.MRData.StandingsTable.StandingsLists[0];
+  positionList = temp.DriverStandings;
+  totalNumDrivers = response.MRData.total;
   return positionList
 });
+
 
 
 class Driver {
@@ -69,18 +34,9 @@ class Driver {
 
 let driverList = [];
 
+
 function addDriver(driverName, driverWins, driverPoints, driverPic, driverBio, interestingContent) {
-  let dName = driverName;
-  let dWins = driverWins;
-  let dPoints = driverPoints;
-
-  for (let i = 0; i < positionList.length; i++) {
-    dName = positionList[0].Driver.givenName + positionList[0].Driver.familyName //first name + last name
-    dPoints = positionList[0].points // no. points
-    dWins = positionList[0].wins // no. wins
-  }
-
-  const driver = new Driver(dName, dWins, dPoints, driverPic, driverBio, interestingContent);
+  const driver = new Driver(driverName, driverWins, driverPoints, driverPic, driverBio, interestingContent);
   driverList.push(driver);
   return driver;
 }
@@ -113,16 +69,40 @@ function updateElement(driver) {
   content.innerText = driver.content;
 }
 
+
+function getInfo() {
+  for (let i = 0; i < totalNumDrivers; i++) {
+    let dName = positionList[i].Driver.givenName + " " + positionList[i].Driver.familyName; //first name + last name
+    let dWins = positionList[i].wins; // no. wins
+    let dPoints = positionList[i].points; // no. points
+
+    let dPic = "./HW1-assets/driverPictures/" + positionList[i].Driver.givenName + ".jpg";
+    let dBio = "hello"
+    let dContent = "beunos Dias"
+    console.log(dName, dWins, dPoints, dPic, dBio, dContent)
+    addDriver(dName, dWins, dPoints, dPic, dBio, dContent);
+  }
+  console.log("hello")
+}
+
+
+
+function loadInfo() {
+  driverList = driverList.reverse()
+  for (let i = 0; i < driverList.length; i++) {
+    console.log(driverList[i]);
+    createElement(driverList[i]);
+  }
+  console.log("there")
+}
+
+getInfo()
+loadInfo()
+
+
+
+
+
 // const mVer = addDriver("M. Verstappen", 14, 429, "./HW1-assets/driverPictures/m_Verstappen.jpg", "hello", "beunos dias")
 // const cPer = addDriver("C. Perez", 14, 429, "./HW1-assets/driverPictures/m_Verstappen.jpg", "hello", "beunos dias")
 // const cLec = addDriver("C. LeClerc", 14, 429, "./HW1-assets/driverPictures/m_Verstappen.jpg", "hello", "beunos dias")
-
-
-
-// for (let i = 0; i < driverList.length; i++) {
-//   //console.log(driverList[i]);
-//   createElement(driverList[i]);
-// }
-
-
-
---
+++ Final-Project/js/universal.js
@@ -0,0 +1,30 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+let isNavOpen = false;
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  // document.getElementById("main").style.margin = "auto";
+  document.getElementById("header").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  // document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("header").style.marginLeft = "0";
+}
+
+
+function navState() {
+  if (isNavOpen === false) {
+    openNav();
+    // console.log("opening Nav");
+    isNavOpen = true;
+  }
+  else {
+    closeNav();
+    // console.log("closing Nav");
+    isNavOpen = false
+  }
+  return isNavOpen
+}
