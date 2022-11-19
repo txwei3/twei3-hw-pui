fac578e26cc73bc8cb9b876d72ac79f519d5e2de
txwei3 <twei3@andrew.cmu.edu>
2022-11-18 23:39:51 -0500

    testing csv

--- Final-Project/js/funViz.js
+++ Final-Project/js/funViz.js
@@ -1,72 +1,171 @@
-var data = [
-    {letter: "A", frequency: .08167},
-    {letter: "B", frequency: .01492},
-    {letter: "C", frequency: .02780},
-    {letter: "D", frequency: .04253},
-    {letter: "E", frequency: .12702},
-    {letter: "F", frequency: .02288},
-    {letter: "G", frequency: .02022},
-    {letter: "H", frequency: .06094},
-    {letter: "I", frequency: .06973},
-    {letter: "J", frequency: .00153},
-    {letter: "K", frequency: .00747},
-    {letter: "L", frequency: .04025},
-    {letter: "M", frequency: .02517},
-    {letter: "N", frequency: .06749},
-    {letter: "O", frequency: .07507},
-    {letter: "P", frequency: .01929},
-    {letter: "Q", frequency: .00098},
-    {letter: "R", frequency: .05987},
-    {letter: "S", frequency: .06333},
-    {letter: "T", frequency: .09056},
-    {letter: "U", frequency: .02758},
-    {letter: "V", frequency: .01037},
-    {letter: "W", frequency: .02465},
-    {letter: "X", frequency: .00150},
-    {letter: "Y", frequency: .01971},
-    {letter: "Z", frequency: .00074}
-  ];
+// var margin = {top: 10, right: 30, bottom: 30, left: 60},
+//     width = 460 - margin.left - margin.right,
+//     height = 400 - margin.top - margin.bottom;
+
+// // append the svg object to the body of the page
+// var svg = d3.select("my_dataviz")
+//   .append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//     .attr("transform",
+//           "translate(" + margin.left + "," + margin.top + ")");
+
+
+// d3.csv("./HW1-assets/csv/dp.csv", function(data) {
+//     // for (let i = 0; i < data.length; i++) {
+//     //     //console.log(data[i])
+//     // }
+//     //console.log(data)
+
+// // Add X axis --> it is a date format
+//     var x = d3.scaleLinear()
+//     .domain([1,22])
+//     .range([ 0, width ]);
+//     svg.append("g")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.axisBottom(x));
+
+//     // Add Y axis
+//     var y = d3.scaleLinear()
+//     .domain([0, 500])
+//     .range([ height, 0 ]);
+//     svg.append("g")
+//     .call(d3.axisLeft(y));
+
+//     // This allows to find the closest X index of the mouse:
+//     var bisect = d3.bisector(function(d) { return d.x; }).left;
+
+//     // Create the circle that travels along the curve of chart
+//     var focus = svg
+//     .append('g')
+//     .append('circle')
+//     .style("fill", "none")
+//     .attr("stroke", "black")
+//     .attr('r', 8.5)
+//     .style("opacity", 0)
+
+//     // Create the text that travels along the curve of chart
+//     var focusText = svg
+//     .append('g')
+//     .append('text')
+//     .style("opacity", 0)
+//     .attr("text-anchor", "left")
+//     .attr("alignment-baseline", "middle")
+
+//     // Add the line
+//     svg
+//     .append("path")
+//     .datum(data)
+//     .attr("fill", "none")
+//     .attr("stroke", "steelblue")
+//     .attr("stroke-width", 1.5)
+//     .attr("d", d3.line()
+//     .x(function(d) { return x(d.x) })
+//     .y(function(d) { return y(d.y) })
+//     )
+// })
+
+
+
 
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-var formatPercent = d3.format(".0%")
-
-
-// var x = d3.scaleOrdinal()
-//     .rangeRoundBands([0, width], .1);
-
-var x = d3.scaleBand()
-    .range([0, width])
-    .round([.1]);
-
-var y = d3.scaleLinear()
-    .range([height, 0]);
-
-
-var xAxis = d3.axisBottom()
-    .scale(x);
-
-var yAxis = d3.axisLeft()
-    .scale(y);
-
+// append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return x(d.letter); })
-    .attr("width", x.bandwidth())
-    .attr("y", function(d) { return y(d.frequency); })
-    .attr("height", function(d) { return height - y(d.frequency); })
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_IC.csv",function(data) {
+
+  // Add X axis --> it is a date format
+  var x = d3.scaleLinear()
+    .domain([1,100])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 13])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // This allows to find the closest X index of the mouse:
+  var bisect = d3.bisector(function(d) { return d.x; }).left;
+
+  // Create the circle that travels along the curve of chart
+  var focus = svg
+    .append('g')
+    .append('circle')
+      .style("fill", "none")
+      .attr("stroke", "black")
+      .attr('r', 8.5)
+      .style("opacity", 0)
+
+  // Create the text that travels along the curve of chart
+  var focusText = svg
+    .append('g')
+    .append('text')
+      .style("opacity", 0)
+      .attr("text-anchor", "left")
+      .attr("alignment-baseline", "middle")
+
+  // Add the line
+  svg
+    .append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x(function(d) { return x(d.x) })
+      .y(function(d) { return y(d.y) })
+      )
+
+  // Create a rect on top of the svg area: this rectangle recovers mouse position
+  svg
+    .append('rect')
+    .style("fill", "none")
+    .style("pointer-events", "all")
+    .attr('width', width)
+    .attr('height', height)
+    .on('mouseover', mouseover)
+    .on('mousemove', mousemove)
+    .on('mouseout', mouseout);
 
 
-      
+  // What happens when the mouse move -> show the annotations at the right positions.
+  function mouseover() {
+    focus.style("opacity", 1)
+    focusText.style("opacity",1)
+  }
 
-//console.log(dtpDictionary)
+  function mousemove() {
+    // recover coordinate we need
+    var x0 = x.invert(d3.mouse(this)[0]);
+    var i = bisect(data, x0, 1);
+    selectedData = data[i]
+    focus
+      .attr("cx", x(selectedData.x))
+      .attr("cy", y(selectedData.y))
+    focusText
+      .html("x:" + selectedData.x + "  -  " + "y:" + selectedData.y)
+      .attr("x", x(selectedData.x)+15)
+      .attr("y", y(selectedData.y))
+    }
+  function mouseout() {
+    focus.style("opacity", 0)
+    focusText.style("opacity", 0)
+  }
+
+})
