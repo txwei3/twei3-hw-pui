659e202803a28b6442339fbe92c628545c4dac94
txwei3 <twei3@andrew.cmu.edu>
2022-11-20 23:51:10 -0500

    all 20 drivers added to csv

--- Final-Project/js/funViz.js
+++ Final-Project/js/funViz.js
@@ -1,6 +1,6 @@
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var margin = {top: 10, right: 30, bottom: 80, left: 60},
+    width = 700 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
