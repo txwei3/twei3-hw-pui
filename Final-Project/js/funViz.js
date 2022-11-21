bfbbb6d01d973e790d17b315224a48b94b7f655a
txwei3 <twei3@andrew.cmu.edu>
2022-11-20 22:06:12 -0500

    graph somewhat functional

--- Final-Project/js/funViz.js
+++ Final-Project/js/funViz.js
@@ -1,74 +1,3 @@
-// var margin = {top: 10, right: 30, bottom: 30, left: 60},
-//     width = 460 - margin.left - margin.right,
-//     height = 400 - margin.top - margin.bottom;
-
-// // append the svg object to the body of the page
-// var svg = d3.select("my_dataviz")
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
-
-
-// d3.csv("./HW1-assets/csv/dp.csv", function(data) {
-//     // for (let i = 0; i < data.length; i++) {
-//     //     //console.log(data[i])
-//     // }
-//     //console.log(data)
-
-// // Add X axis --> it is a date format
-//     var x = d3.scaleLinear()
-//     .domain([1,22])
-//     .range([ 0, width ]);
-//     svg.append("g")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(x));
-
-//     // Add Y axis
-//     var y = d3.scaleLinear()
-//     .domain([0, 500])
-//     .range([ height, 0 ]);
-//     svg.append("g")
-//     .call(d3.axisLeft(y));
-
-//     // This allows to find the closest X index of the mouse:
-//     var bisect = d3.bisector(function(d) { return d.x; }).left;
-
-//     // Create the circle that travels along the curve of chart
-//     var focus = svg
-//     .append('g')
-//     .append('circle')
-//     .style("fill", "none")
-//     .attr("stroke", "black")
-//     .attr('r', 8.5)
-//     .style("opacity", 0)
-
-//     // Create the text that travels along the curve of chart
-//     var focusText = svg
-//     .append('g')
-//     .append('text')
-//     .style("opacity", 0)
-//     .attr("text-anchor", "left")
-//     .attr("alignment-baseline", "middle")
-
-//     // Add the line
-//     svg
-//     .append("path")
-//     .datum(data)
-//     .attr("fill", "none")
-//     .attr("stroke", "steelblue")
-//     .attr("stroke-width", 1.5)
-//     .attr("d", d3.line()
-//     .x(function(d) { return x(d.x) })
-//     .y(function(d) { return y(d.y) })
-//     )
-// })
-
-
-
-
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
@@ -83,89 +12,68 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_IC.csv",function(data) {
+d3.csv("./HW1-assets/csv/test.csv", function(data) {
 
-  // Add X axis --> it is a date format
-  var x = d3.scaleLinear()
-    .domain([1,100])
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+  // console.log(data[0].year)
+  // console.log(data[1].year)
+  //let raceName = ["bharain", "SA", "australia", "emilia", "miami"]
+
+  // for (let i = 0; i < data.length; i++) {
+  //   if (!(data[i].year in raceName)) {
+  //     console.log('hello');
+  //     raceName.push(data[i].year)
+  //   }
+  //   else {
+  //     console.log('ok')
+  //   }
+  // }
+  //console.log(raceName)
+
+
+  // group the data: I want to draw one line per group
+  var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+    .key(function(d) { return d.driver;})
+    .entries(data);
+
+
+var x = d3.scaleBand()
+  .domain(data.map(function(d) { return d.race; }))
+  .range([ 0, width ]);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x).ticks(5))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 13])
+    .domain([0, d3.max(data, function(d) { return +d.n; })])
     .range([ height, 0 ]);
   svg.append("g")
     .call(d3.axisLeft(y));
 
-  // This allows to find the closest X index of the mouse:
-  var bisect = d3.bisector(function(d) { return d.x; }).left;
+  // color palette
+  var res = sumstat.map(function(d){ return d.key }) // list of group names
+  var color = d3.scaleOrdinal()
+    .domain(res)
+    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
 
-  // Create the circle that travels along the curve of chart
-  var focus = svg
-    .append('g')
-    .append('circle')
-      .style("fill", "none")
-      .attr("stroke", "black")
-      .attr('r', 8.5)
-      .style("opacity", 0)
-
-  // Create the text that travels along the curve of chart
-  var focusText = svg
-    .append('g')
-    .append('text')
-      .style("opacity", 0)
-      .attr("text-anchor", "left")
-      .attr("alignment-baseline", "middle")
-
-  // Add the line
-  svg
-    .append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("d", d3.line()
-      .x(function(d) { return x(d.x) })
-      .y(function(d) { return y(d.y) })
-      )
-
-  // Create a rect on top of the svg area: this rectangle recovers mouse position
-  svg
-    .append('rect')
-    .style("fill", "none")
-    .style("pointer-events", "all")
-    .attr('width', width)
-    .attr('height', height)
-    .on('mouseover', mouseover)
-    .on('mousemove', mousemove)
-    .on('mouseout', mouseout);
-
-
-  // What happens when the mouse move -> show the annotations at the right positions.
-  function mouseover() {
-    focus.style("opacity", 1)
-    focusText.style("opacity",1)
-  }
-
-  function mousemove() {
-    // recover coordinate we need
-    var x0 = x.invert(d3.mouse(this)[0]);
-    var i = bisect(data, x0, 1);
-    selectedData = data[i]
-    focus
-      .attr("cx", x(selectedData.x))
-      .attr("cy", y(selectedData.y))
-    focusText
-      .html("x:" + selectedData.x + "  -  " + "y:" + selectedData.y)
-      .attr("x", x(selectedData.x)+15)
-      .attr("y", y(selectedData.y))
-    }
-  function mouseout() {
-    focus.style("opacity", 0)
-    focusText.style("opacity", 0)
-  }
+  // Draw the line
+  svg.selectAll(".line")
+      .data(sumstat)
+      .enter()
+      .append("path")
+        .attr("fill", "none")
+        .attr("stroke", function(d){ return color(d.key) })
+        .attr("stroke-width", 1.5)
+        .attr("d", function(d){
+          return d3.line()
+            .x(function(d) { return x(d.race); })
+            .y(function(d) { return y(+d.n); })
+            (d.values)
+        })
 
 })
